acae8d81205fb5cde73558c54159fe669d9c90de
claeis <ce@eisenhutinformatik.ch>
2021-04-14 15:33:43 +0200

    filter kantPersNr

--- src/ch/ehi/gbdbs4bfs/Gbdbs4bfs.java
+++ src/ch/ehi/gbdbs4bfs/Gbdbs4bfs.java
@@ -1,7 +1,10 @@
 package ch.ehi.gbdbs4bfs;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Set;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
@@ -24,6 +27,7 @@ import ch.ehi.basics.logging.FileListener;
 import ch.ehi.basics.logging.StdListener;
 import ch.ehi.basics.settings.Settings;
 import ch.ehi.gbdbs4bfs.jaxb.gbbasistypen._2_1.InhaltNatuerlichePersonGBType;
+import ch.ehi.gbdbs4bfs.jaxb.gbbasisid._2_1.PersonIdDefType;
 import ch.ehi.gbdbs4bfs.jaxb.gbbasistypen._2_1.AnmeldungType;
 import ch.ehi.gbdbs4bfs.jaxb.gbbasistypen._2_1.BergwerkType;
 import ch.ehi.gbdbs4bfs.jaxb.gbbasistypen._2_1.EigentumAnteilType;
@@ -83,9 +87,9 @@ public class Gbdbs4bfs {
     public static final QName DATASECTION_21 = new QName(gbdbsDatei21Ns,"DATASECTION");
     public static final QName VOLLBESTAND_21 = new QName(gbdbsDatei21Ns,"Vollbestand");
     private Marshaller ms = null;
-    private XMLEventWriter out = null;
     private ch.ehi.gbdbs4bfs.jaxb.gbbasistypen._2_1.ObjectFactory of=null;
     private long personIdx=0;
+    private Set<String> egbpids=new java.util.HashSet<String>();
 	public static boolean convert(
 			File fullFile,
 			File reducedFile,
@@ -130,113 +134,21 @@ public class Gbdbs4bfs {
             EhiLogger.logState("reducedFile <"+reducedFile.getPath()+">");
 		
 			// process data files
-			EhiLogger.logState("convert data...");
-		    XMLInputFactory xmlif=null;
-		    XMLOutputFactory xmlof = null;
-		    XMLEventFactory xmlef=null;
-		    Unmarshaller um=null;
-		    XMLEventReader xmlr = null;
-            java.io.OutputStream reducedStream = null;
-            java.io.InputStream fullStream = null;
-			try{
-				
-		        xmlif = XMLInputFactory.newInstance();
-		        xmlof = XMLOutputFactory.newInstance();
-                xmlef = XMLEventFactory.newInstance();
-		          try {
-		              JAXBContext jaxbContext = JAXBContext
-		              .newInstance(JAXB_CONTEXT_PATH);
-		              um = jaxbContext.createUnmarshaller();
-		              ms = jaxbContext.createMarshaller();
-		              ms.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
-		          } catch (JAXBException e) {
-		              throw new IllegalStateException(e);
-		          }
-                reducedStream = new java.io.BufferedOutputStream(new java.io.FileOutputStream(reducedFile));
-                out = xmlof.createXMLEventWriter(reducedStream, "UTF-8");
-                fullStream = new java.io.BufferedInputStream(new java.io.FileInputStream(fullFile));
-                xmlr = xmlif.createXMLEventReader(fullStream);
-                XMLEvent ev = null;
-	            // move to the root element and check its name.
-	            do{
-	                ev=xmlr.nextEvent();
-	            }while(!(ev instanceof StartElement));
-	            if(!ev.asStartElement().getName().equals(GBDBS_21)) { 
-	                EhiLogger.logError("not a GBDBS 2.1 transfer file (" +ev.asStartElement().getName()+")");
-	            }else{
-	                out.add(xmlef.createStartDocument());
-	                out.add(ev);
-	                ev=xmlr.nextEvent();
-	                ev=skipSpacesAndGetNextEvent(xmlr, ev);
-	                if(ev.asStartElement().getName().equals(HEADERSECTION_21)) {
-	                    out.add(ev);
-	                    skip(xmlr);
-	                    out.add(xmlef.createEndElement(HEADERSECTION_21,null));
-	                }
-	                ev=xmlr.nextEvent();
-	                ev=skipSpacesAndGetNextEvent(xmlr, ev);
-	                if(ev.asStartElement().getName().equals(DATASECTION_21)) {
-	                    out.add(ev);
-	                    ev=xmlr.nextEvent();
-	                    ev=skipSpacesAndGetNextEvent(xmlr, ev);
-	                    if(ev.asStartElement().getName().equals(VOLLBESTAND_21)) {
-	                        out.add(ev);
-	                        skipSpaces(xmlr);
-	                        while(xmlr.peek().isStartElement()) {
-	                            JAXBElement ele=(JAXBElement) um.unmarshal(xmlr);
-	                            convertGbdbsEle(ele);
-	                            skipSpaces(xmlr);
-	                        }
-	                        out.add(xmlef.createEndElement(VOLLBESTAND_21,null));
-	                    }
-                        out.add(xmlef.createEndElement(DATASECTION_21,null));
-	                }
-                    out.add(xmlef.createEndElement(GBDBS_21,null));
-                    out.add(xmlef.createEndDocument());
-	            }
-		        
-
-	            
-				// check for errors
-				if(StdListener.getInstance().hasSeenErrors()){
-					EhiLogger.logState("...conversion failed");
-				}else{
-					EhiLogger.logState("...conversion done");
-					ret=true;
-				}
-			}catch(Throwable ex){
-				EhiLogger.logError(ex);
-				EhiLogger.logState("...conversion failed");
-			}finally {
-			    if(out!=null) {
-	                try {
-                        out.close();
-                    } catch (XMLStreamException e) {
-                    }
-	                out=null;
-			    }
-			    if(xmlr!=null) {
-	                try {
-                        xmlr.close();
-                    } catch (XMLStreamException e) {
-                    }
-	                xmlr=null;
-			    }
-			    if(fullStream!=null) {
-	                try {
-                        fullStream.close();
-                    } catch (IOException e) {
-                    }
-	                fullStream=null;
-			    }
-			    if(reducedStream!=null) {
-	                try {
-                        reducedStream.close();
-                    } catch (IOException e) {
-                    }
-	                reducedStream=null;
-			    }
-			}
+            EhiLogger.logState("convert data...");
+            try {
+                pass1(fullFile);
+                ret = pass2(fullFile, reducedFile);
+                // check for errors
+                if(StdListener.getInstance().hasSeenErrors()){
+                    EhiLogger.logState("...conversion failed");
+                }else{
+                    EhiLogger.logState("...conversion done");
+                    ret=true;
+                }
+            }catch(Throwable ex){
+                EhiLogger.logError(ex);
+                EhiLogger.logState("...conversion failed");
+            }
 		}finally{
 			if(logfile!=null){
 				logfile.close();
@@ -246,24 +158,196 @@ public class Gbdbs4bfs {
 		}
 		return ret;
 	}
-    private void convertGbdbsEle(JAXBElement ele) throws JAXBException {
+    private void pass1(File fullFile) throws XMLStreamException, IOException, JAXBException {
+        XMLInputFactory xmlif=null;
+        XMLEventFactory xmlef=null;
+        Unmarshaller um=null;
+        XMLEventReader xmlr = null;
+        java.io.InputStream fullStream = null;
+        try{
+        	
+            xmlif = XMLInputFactory.newInstance();
+            xmlef = XMLEventFactory.newInstance();
+              try {
+                  JAXBContext jaxbContext = JAXBContext
+                  .newInstance(JAXB_CONTEXT_PATH);
+                  um = jaxbContext.createUnmarshaller();
+              } catch (JAXBException e) {
+                  throw new IllegalStateException(e);
+              }
+            fullStream = new java.io.BufferedInputStream(new java.io.FileInputStream(fullFile));
+            xmlr = xmlif.createXMLEventReader(fullStream);
+            XMLEvent ev = null;
+            // move to the root element and check its name.
+            do{
+                ev=xmlr.nextEvent();
+            }while(!(ev instanceof StartElement));
+            if(!ev.asStartElement().getName().equals(GBDBS_21)) { 
+                throw new JAXBException("not a GBDBS 2.1 transfer file (" +ev.asStartElement().getName()+")");
+            }else{
+                ev=xmlr.nextEvent();
+                ev=skipSpacesAndGetNextEvent(xmlr, ev);
+                if(ev.asStartElement().getName().equals(HEADERSECTION_21)) {
+                    skip(xmlr);
+                }
+                ev=xmlr.nextEvent();
+                ev=skipSpacesAndGetNextEvent(xmlr, ev);
+                if(ev.asStartElement().getName().equals(DATASECTION_21)) {
+                    ev=xmlr.nextEvent();
+                    ev=skipSpacesAndGetNextEvent(xmlr, ev);
+                    if(ev.asStartElement().getName().equals(VOLLBESTAND_21)) {
+                        skipSpaces(xmlr);
+                        while(xmlr.peek().isStartElement()) {
+                            JAXBElement ele=(JAXBElement) um.unmarshal(xmlr);
+                            if(ele.getValue() instanceof PersonGBType){
+                                String gbdbsId=((PersonGBType)ele.getValue()).getNummer();
+                                String egbpid=getEGBPID(gbdbsId);
+                                egbpids.add(egbpid);
+                            }
+                            skipSpaces(xmlr);
+                        }
+                    }
+                }
+            }
+        }finally {
+            if(xmlr!=null) {
+                try {
+                    xmlr.close();
+                } catch (XMLStreamException e) {
+                }
+                xmlr=null;
+            }
+            if(fullStream!=null) {
+                try {
+                    fullStream.close();
+                } catch (IOException e) {
+                }
+                fullStream=null;
+            }
+        }
+    }
+    
+    private boolean pass2(File fullFile, File reducedFile) throws XMLStreamException, IOException, JAXBException {
+        boolean ret=false;
+        XMLInputFactory xmlif=null;
+        XMLOutputFactory xmlof = null;
+        XMLEventFactory xmlef=null;
+        Unmarshaller um=null;
+        XMLEventReader xmlr = null;
+        java.io.OutputStream reducedStream = null;
+        java.io.InputStream fullStream = null;
+        XMLEventWriter out = null;
+        try{
+            
+            xmlif = XMLInputFactory.newInstance();
+            xmlof = XMLOutputFactory.newInstance();
+            xmlef = XMLEventFactory.newInstance();
+              try {
+                  JAXBContext jaxbContext = JAXBContext
+                  .newInstance(JAXB_CONTEXT_PATH);
+                  um = jaxbContext.createUnmarshaller();
+                  ms = jaxbContext.createMarshaller();
+                  ms.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+              } catch (JAXBException e) {
+                  throw new IllegalStateException(e);
+              }
+            reducedStream = new java.io.BufferedOutputStream(new java.io.FileOutputStream(reducedFile));
+            out = xmlof.createXMLEventWriter(reducedStream, "UTF-8");
+            fullStream = new java.io.BufferedInputStream(new java.io.FileInputStream(fullFile));
+            xmlr = xmlif.createXMLEventReader(fullStream);
+            XMLEvent ev = null;
+            // move to the root element and check its name.
+            do{
+                ev=xmlr.nextEvent();
+            }while(!(ev instanceof StartElement));
+            if(!ev.asStartElement().getName().equals(GBDBS_21)) { 
+                EhiLogger.logError("not a GBDBS 2.1 transfer file (" +ev.asStartElement().getName()+")");
+            }else{
+                out.add(xmlef.createStartDocument());
+                out.add(ev);
+                ev=xmlr.nextEvent();
+                ev=skipSpacesAndGetNextEvent(xmlr, ev);
+                if(ev.asStartElement().getName().equals(HEADERSECTION_21)) {
+                    out.add(ev);
+                    skip(xmlr);
+                    out.add(xmlef.createEndElement(HEADERSECTION_21,null));
+                }
+                ev=xmlr.nextEvent();
+                ev=skipSpacesAndGetNextEvent(xmlr, ev);
+                if(ev.asStartElement().getName().equals(DATASECTION_21)) {
+                    out.add(ev);
+                    ev=xmlr.nextEvent();
+                    ev=skipSpacesAndGetNextEvent(xmlr, ev);
+                    if(ev.asStartElement().getName().equals(VOLLBESTAND_21)) {
+                        out.add(ev);
+                        skipSpaces(xmlr);
+                        while(xmlr.peek().isStartElement()) {
+                            JAXBElement ele=(JAXBElement) um.unmarshal(xmlr);
+                            convertGbdbsEle(ele,out);
+                            skipSpaces(xmlr);
+                        }
+                        out.add(xmlef.createEndElement(VOLLBESTAND_21,null));
+                    }
+                    out.add(xmlef.createEndElement(DATASECTION_21,null));
+                }
+                out.add(xmlef.createEndElement(GBDBS_21,null));
+                out.add(xmlef.createEndDocument());
+            }
+        }finally {
+            if(out!=null) {
+                try {
+                    out.close();
+                } catch (XMLStreamException e) {
+                }
+                out=null;
+            }
+            if(xmlr!=null) {
+                try {
+                    xmlr.close();
+                } catch (XMLStreamException e) {
+                }
+                xmlr=null;
+            }
+            if(fullStream!=null) {
+                try {
+                    fullStream.close();
+                } catch (IOException e) {
+                }
+                fullStream=null;
+            }
+            if(reducedStream!=null) {
+                try {
+                    reducedStream.close();
+                } catch (IOException e) {
+                }
+                reducedStream=null;
+            }
+        }
+        return ret;
+    }
+    private void convertGbdbsEle(JAXBElement ele,XMLEventWriter out) throws JAXBException {
         if(ele.getValue() instanceof GrundstueckType){
-            convertGrundstueck(ele);
+            convertGrundstueck(ele,out);
         }else if(ele.getValue() instanceof RechtType){
-            convertRecht(ele);
+            convertRecht(ele,out);
         }else if(ele.getValue() instanceof PersonGBType){
-            convertPerson(ele);
+            convertPerson(ele,out);
         }else if(ele.getValue() instanceof AnmeldungType){
         }else{
             throw new IllegalArgumentException("unexpected type "+ele.getValue().getClass());
         }
     }
-    private void convertPerson(JAXBElement<? extends PersonGBType> ele) throws JAXBException {
+    private void convertPerson(JAXBElement<? extends PersonGBType> ele,XMLEventWriter out) throws JAXBException {
         personIdx++;
         JAXBElement<? extends PersonGBType> newele=null;
         if(ele.getValue() instanceof GemeinschaftType) {
             GemeinschaftType val=new GemeinschaftType();
             for(PersonRefBegruendet mitglied:((GemeinschaftType)ele.getValue()).getMitglieder()) {
+                String nummer=mitglied.getRef();
+                PersonIdDefType berechtigteId = parseGbdbsPersId(nummer);
+                berechtigteId.setKantPersNr(null);
+                nummer=toString(berechtigteId);
+                mitglied.setRef(nummer);
                 val.getMitglieder().add(mitglied);
             }
             newele=of.createGemeinschaft(val);
@@ -335,16 +419,29 @@ public class Gbdbs4bfs {
                 }
                 newele.getValue().getInhaltPersonGB().add(newinhalt);
             }
-            newele.getValue().setNummer(ele.getValue().getNummer());
+            String nummer=ele.getValue().getNummer();
+            PersonIdDefType berechtigteId = parseGbdbsPersId(nummer);
+            if(egbpids.contains(berechtigteId.getEGBPID())) {
+                berechtigteId.setKantPersNr(null);
+                nummer=toString(berechtigteId);
+            }
+            newele.getValue().setNummer(nummer);
             ms.marshal(newele, out);
         }
     }
-    private void convertRecht(JAXBElement<? extends RechtType> value) throws JAXBException {
+    private void convertRecht(JAXBElement<? extends RechtType> value,XMLEventWriter out) throws JAXBException {
         if(value.getValue() instanceof EigentumAnteilType) {
+            EigentumAnteilType eig=(EigentumAnteilType)value.getValue();
+            String berechtigteGbdbsId=eig.getBerechtigte();
+            PersonIdDefType berechtigteId = parseGbdbsPersId(berechtigteGbdbsId);
+            if(egbpids.contains(berechtigteId.getEGBPID())) {
+                berechtigteId.setKantPersNr(null);
+                eig.setBerechtigte(toString(berechtigteId));
+            }
             ms.marshal(value, out);
         }
     }
-    private void convertGrundstueck(JAXBElement<? extends GrundstueckType> ele) throws JAXBException {
+    private void convertGrundstueck(JAXBElement<? extends GrundstueckType> ele,XMLEventWriter out) throws JAXBException {
         JAXBElement<? extends GrundstueckType> newele=null;
         if(ele.getValue() instanceof BergwerkType) {
             BergwerkType val=new BergwerkType();
@@ -381,6 +478,43 @@ public class Gbdbs4bfs {
             newele.getValue().setGemeinde(ele.getValue().getGemeinde());
             ms.marshal(newele, out);
         }
+    }
+    private String getEGBPID(String gbdbsId) {
+        String egbpid =parseGbdbsPersId(gbdbsId).getEGBPID();
+        return egbpid;
+    }
+    static public PersonIdDefType parseGbdbsPersId(String gbdbsId) {
+        PersonIdDefType ret = new PersonIdDefType();
+        String idv[] = gbdbsId.split(":", 6);
+        String egbpid = idv[0].trim();
+        if (egbpid.length() > 0) {
+            ret.setEGBPID(idv[0]);
+        }
+        String kantPersNr = idv[1].trim();
+        if (kantPersNr.length() > 0) {
+            ret.setKantPersNr(kantPersNr);
+        }
+        String uid = idv[2].trim();
+        if (uid.length() > 0) {
+            ret.setUID(uid);
+        }
+        String ahvn13 = idv[3].trim();
+        if (ahvn13.length() > 0) {
+            ret.setAHVN13(ahvn13);
+        }
+        String localId = idv[4].trim();
+        if (localId.length() > 0) {
+            ret.setLocalId(localId);
+        }
+        return ret;
+    }
+    public static String toString(PersonIdDefType gbdbsId) {
+        return (gbdbsId.getEGBPID()!=null?gbdbsId.getEGBPID():"")
+        +":"+(gbdbsId.getKantPersNr()!=null?gbdbsId.getKantPersNr():"")
+        +":"+(gbdbsId.getUID()!=null?gbdbsId.getUID():"")
+        +":"+(gbdbsId.getAHVN13()!=null?gbdbsId.getAHVN13():"")
+        +":"+(gbdbsId.getLocalId()!=null?gbdbsId.getLocalId():"")
+        ;
     }
     private XMLEvent skipSpacesAndGetNextEvent(XMLEventReader reader,XMLEvent event) throws XMLStreamException{
         while(event.isCharacters() || event.getEventType()==XMLEvent.COMMENT){
